1e5ad6ae1ad6dfd67502caf226b226719b26b673
ahmadadnan16 <ahmadadnan1602@gmail.com>
2024-01-16 19:51:55 +0530

    Url channge

--- assignment-linkbroker/src/App.jsx
+++ assignment-linkbroker/src/App.jsx
@@ -1,21 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage1 from './components/LandingPage1';
-import Card from './components/Card';
-import Poppage from './components/PopImg';
+
 
 function App() {
   return (
   <>
     
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<LandingPage1 />} />
-  <Route path="/card" element={<Card/>} />
-   <Route path='/popimg' element={<Poppage/>}/>
-</Routes>
-</BrowserRouter>
+<LandingPage1/>
 </>
   );
 }
